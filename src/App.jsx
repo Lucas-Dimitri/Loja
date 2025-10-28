5830308083b27ee1c7131820b887145d90143813
Lucas-Dimitri <lucas.ferreirasoares@hotmail.com>
2025-10-28 14:47:47 -0300

    returning to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./bs-customizacao.css";
 import Home from "./pages/Home";
 import Produto from "./pages/Produto";
