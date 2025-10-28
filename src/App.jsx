51684ae3fe23f40ff3be0aa9fe9684c171f62d83
Lucas-Dimitri <lucas.ferreirasoares@hotmail.com>
2025-10-28 14:49:47 -0300

    changing some bootstrap config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./bs-customizacao.css";
 import Home from "./pages/Home";
 import Produto from "./pages/Produto";
 import Carrinho from "./pages/Carrinho";
