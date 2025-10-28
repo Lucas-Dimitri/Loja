6e745dd93d60c0bf8e0febe209faf3fd7d7e8413
Lucas-Dimitri <lucas.ferreirasoares@hotmail.com>
2025-10-28 15:08:38 -0300

    solving gh pages with bs file

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import "./bs-customizacao.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 /* 
@@ -16,8 +15,8 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
     4.3. filtrar compras com algum produto especifico = true
 */
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <App /> 
-  </StrictMode>,
-)
+    <App />
+  </StrictMode>
+);
